aa5f95a66964cf1602943deeafc6cceaddde4fc4
MachiHat <mati01herrera@gmail.com>
2022-04-18 20:20:34 -0300

    yea we doing this

--- src/components/TopNav.jsx
+++ src/components/TopNav.jsx
@@ -3,42 +3,12 @@ import React from "react";
 export const TopNav = () => {
   return (
     <div className="navbar bg-primary text-primary-content">
-      <div className="flex-1">
-        <button className="btn btn-ghost normal-case text-xl">
-          LA IMPRESIONANTE LIBRERIA DE{" "}
-          <span className="text-secondary text-xxl">MACHI</span>
+      <div className="flex-1 min-w-fit">
+        <button className="btn btn-ghost normal-case text-lg">
+          LIBRERIA
+          <span className="text-info font-black ml-2">MACHI</span>
         </button>
       </div>
-      <ul className="menu menu-horizontal p-0">
-        <li>
-          <button>Item 1</button>
-        </li>
-        <li tabindex="0">
-          <button>
-            Parent
-            <svg
-              className="fill-current"
-              xmlns="http://www.w3.org/2000/svg"
-              width="20"
-              height="20"
-              viewBox="0 0 24 24"
-            >
-              <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
-            </svg>
-          </button>
-          <ul className="p-2 bg-primary">
-            <li>
-              <button>Submenu 1</button>
-            </li>
-            <li>
-              <button>Submenu 2</button>
-            </li>
-          </ul>
-        </li>
-        <li>
-          <button>Item 3</button>
-        </li>
-      </ul>
     </div>
   );
 };
--- src/pages/BookList.jsx
+++ src/pages/BookList.jsx
@@ -1,18 +1,38 @@
 import React from "react";
 import { useEffect, useState } from "react";
+// import { Link } from "react-router-dom";
 
 import { BookCard } from "../components/BookCard";
 import { API_KEY } from "../data";
 
-export const BookList = ( searchParams ) => {
+export const BookList = () => {
+  const bookTitles = [
+    "El Principito",
+    "El Retrato de Dorian Gray",
+    "Farenheit 451",
+    "100 Años de Soledad",
+    "Romancero Gitano",
+  ];
+  
+  const [searchParams, setSearchParams] = useState();
+  
+  const [bookTitle, setBookTitle] = useState();
 
-  console.log(searchParams);
+  const [ randomTitle ] = useState(bookTitles[Math.floor(Math.random()*bookTitles.length)]);
+  
+  const startSearch = (e) => {
+    e.preventDefault();
+    setBookTitle(searchParams);
+    e.target.reset();
+  }
 
   const [data, setData] = useState([]);
 
   useEffect(() => {
     fetch(
-      `https://www.googleapis.com/books/v1/volumes?q=${searchParams}&filter=paid-ebooks&langResttrict=es&printType=books&maxResults=5&key=${API_KEY}`
+      `https://www.googleapis.com/books/v1/volumes?q=${
+        bookTitle ?? randomTitle
+      }&filter=paid-ebooks&langResttrict=es&printType=books&maxResults=5&key=${API_KEY}`
     )
       .then((response) => {
         return response.json();
@@ -23,22 +43,50 @@ export const BookList = ( searchParams ) => {
       .catch((error) => {
         console.error(error);
       });
-  }, [searchParams]);
+  }, [bookTitle, randomTitle]);
 
   return (
     <div>
-      {data.map((book, i) => (
-        <BookCard
-          key={i}
-          img={book.volumeInfo?.imageLinks?.thumbnail}
-          author={book.volumeInfo?.authors[0]}
-          title={book.volumeInfo.title}
-          publishedDate={book.volumeInfo.publishedDate}
-          publisher={book.volumeInfo.publisher}
-          price={book.saleInfo.listPrice.amount}
-          buyLink={book.saleInfo.buyLink}
-        />
-      ))}
+      <form onSubmit={startSearch} className="form-control">
+        <div className="input-group">
+          <input
+            onChange={e => setSearchParams(e.target.value)}
+            type="text"
+            placeholder="Buscar..."
+            className="input input-bordered input-primary w-full max-w-xs"
+          />
+          <button type='submit' className="btn btn-square btn-primary">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              />
+            </svg>
+          </button>
+        </div>
+      </form>
+      <div>
+        {data.map((book, i) => (
+          <BookCard
+            key={i}
+            img={book.volumeInfo?.imageLinks?.thumbnail}
+            author={book.volumeInfo?.authors[0]}
+            title={book.volumeInfo.title}
+            publishedDate={book.volumeInfo.publishedDate}
+            publisher={book.volumeInfo.publisher}
+            price={book.saleInfo.listPrice.amount}
+            buyLink={book.saleInfo.buyLink}
+          />
+        ))}
+      </div>
     </div>
   );
 };
